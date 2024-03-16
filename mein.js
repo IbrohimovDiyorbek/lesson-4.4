d2dbc58d522ea180f9ccaf1b31c27e16eae568e1
IbrohimovDiyorbek <diyorbekibrohimov31@gmail.com>
2024-03-16 22:00:46 +0500

    it is a new homework

---
+++ mein.js
@@ -0,0 +1,11 @@
+let random1 = Math.trunc(101 * Math.random());
+
+if (random1 % 3 == 0) {
+    alert(random1 + " - bu son fizz")
+} else if (random1 % 5 == 0) {
+    alert(random1 + " - bu son buzz")
+} else if (random1 % 3 == 0 && random1 % 5 == 0) {
+    alert(random1 + " - bu son fizzbuzz")
+} else {
+    alert(random1 + " - bu son fizz, buzz va fizzbuzz ham emas!😞")
+}
